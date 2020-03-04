6f10b5eabe70d3922234bcc63709600ec94f6d32
ando <finaritra017@gmail.com>
2020-03-04 12:23:11 +0400

    ajouter dina

---
+++ mulTwolnt.py
@@ -0,0 +1,8 @@
+def mul(x,y):
+	return( x * y)
+x = int(input("Entre une valeur: "))
+y = int(input("Entre une autre valeur: "))
+print("voila la multiplication de votre valeur")
+
+if __name__ == "__main__":
+	print(mul(x,y))
