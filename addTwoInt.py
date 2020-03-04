cce2c00963d81108005b3857c4c5d904ae5e3feb
Dina64 <randrianaivo64@gmail.com>
2020-03-04 12:35:31 +0400

    Ajout de Dina

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,8 +1,10 @@
-def add(a,b):
-	c = a+b
-	return(c)
-if __name__ == "__main__":
-	a = int(input("Entrer un entier: "))
-	b = int(input("Endrer un entier: "))
-	print(add(a,b))
+#!/usr/bin/env python
 
+import sys
+
+print(sys.argv)
+
+x = int( sys.argv[1] )
+y = int( sys.argv[2] )
+
+print(x+y)
