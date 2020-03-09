c434fbf8b01427f493a591df5b24998d1fa3ba6c
ando <finaritra017@gmail.com>
2020-03-09 15:25:07 +0400

    ajout de ram

--- mulTwonlt.py
+++ mulTwonlt.py
@@ -7,7 +7,15 @@ if __name__ == "__main__":
 	elif len(sys.argv) == 3:
 		x = int(sys.argv[1])
 		y = int(sys.argv[2])
-		print(("x * y = ")(x*y))
-	else:
+		print(("x * y = "),(x*y))
+	
+	elif len(sys.argv)==2:
 		print("peu d'argument")
-
+		x = int(sys.argv[1])
+		y = int(input("inserez le 2eme valeur: "))
+		print((x), "*",(y) ,"= ",(x*y))
+	else:
+		print("ajouter des valeur")
+		x  = int(input("ajoutez 1er valeur: "))
+		y  = int(input("ajoutez la 2eme valeur: "))
+		print((x),"*",(y),"=",(x*y))
