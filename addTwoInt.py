3a94adae59f29128c736e266103c9282a436d185
Dina64 <randrianaivo64@gmail.com>
2020-03-09 10:53:58 +0400

    ajout de Dina

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,10 +1,15 @@
+
+
 #!/usr/bin/env python
 
 import sys
 
 print(sys.argv)
-
-x = int( sys.argv[1] )
-y = int( sys.argv[2] )
-
-print(x+y)
+while true:
+    try:
+	x = int( sys.argv[1] )
+	y = int( sys.argv[2] )
+            print((x)," + ",(y)," = ",(x+y))
+            break
+     except ValueError:
+        print("Erreur,entrer pas plus de deux variable!")
