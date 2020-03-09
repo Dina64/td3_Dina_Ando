f5d5d3e7c55e6e7f535fbd8439a35a8960142d37
Dina64 <randrianaivo64@gmail.com>
2020-03-09 14:37:22 +0400

    modification pour le msg d'erreur

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-
-print(sys.argv)
-x = int( sys.argv[1] )
-y = int( sys.argv[2] )
-print((x)," + ",(y)," = ",(x+y)) 
-print("toto")
+if __name__ == "__main__":
+	if len(sys.argv)>3:
+		print("Veillez inserer que deux arguments")
+	elif len(sys.argv)==3:
+		x = int( sys.argv[1] )
+		y = int( sys.argv[2] )
+		print((x)," + ",(y)," = ",(x+y))
+	else: 
+		print("Trop peu d'argument")
